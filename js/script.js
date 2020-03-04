9c73412ada7607232f1d805c4a514293cfc69f1d
Malgorzata Kolakowska <m.a.kolakowska@googlemail.com>
2020-03-04 17:56:09 +0000

    Add the variable for paper

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let computerMove = "rock";
+
+printMessage("I played " + computerMove + "! If your move is paper, you win!");
+
+let playerMove = "paper";
+
+printMessage("I played " + playerMove + ". I win!");
