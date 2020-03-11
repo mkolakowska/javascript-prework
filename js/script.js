48729fa32b9f9de1202b0dcb25a7ea9c89491722
Malgorzata Kolakowska <m.a.kolakowska@googlemail.com>
2020-03-11 18:06:22 +0000

    Correct the code by adding const

--- js/script.js
+++ js/script.js
@@ -27,85 +27,85 @@ window.onclick = function(event) {
   }
 };
 
-const score = document.getElementById("score");
-const restart = document.getElementById("restart");
-const updateScore = {
-  pScore: 0,
-  cScore: 0
-};
-const playGame = function(playerInput) {
-  clearMessages();
-  const getMoveName = function(argMoveId) {
-    if (argMoveId == 1) {
-      return "rock";
-    } else if (argMoveId == 2) {
-      return "paper";
-    } else if (argMoveId == 3) {
-      return "scissors";
-    }
+//Game
+const game = () => {
+  const score = document.getElementById("score");
+
+  const updateScore = {
+    pScore: 0,
+    cScore: 0
   };
 
-  const randomNumber = Math.floor(Math.random() * 3 + 1);
+  //Play Game
+  const playGame = function(playerInput) {
+    clearMessages();
+    const getMoveName = function(argMoveId) {
+      if (argMoveId == 1) {
+        return "rock";
+      } else if (argMoveId == 2) {
+        return "paper";
+      } else if (argMoveId == 3) {
+        return "scissors";
+      }
+    };
 
-  console.log("The drawn number is: " + randomNumber);
+    //Computer Options
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
+    const computerMove = getMoveName(randomNumber);
+    const playerMove = getMoveName(playerInput);
 
-  const computerMove = getMoveName(randomNumber);
+    //Compare Hands
+    const displayResult = function(argComputerMove, argPlayerMove) {
+      printMessage(
+        "I played " + argComputerMove + ", and you " + argPlayerMove
+      );
+      if (
+        (computerMove == "rock" && playerMove == "paper") ||
+        (computerMove == "scissors" && playerMove == "rock") ||
+        (computerMove == "paper" && playerMove == "scissors")
+      ) {
+        printMessage("You Win");
+        updateScore.pScore++;
+      } else if (
+        (computerMove == "paper" && playerMove == "rock") ||
+        (computerMove == "rock" && playerMove == "scissors") ||
+        (computerMove == "scissors" && playerMove == "paper")
+      ) {
+        printMessage("I Win");
+        updateScore.cScore++;
+      } else if (computerMove === playerMove) {
+        printMessage("It's A Draw");
+      }
+    };
+    displayResult(computerMove, playerMove);
 
-  /*printMessage("My move is: " + computerMove);*/
-
-  console.log("Player entered: " + playerInput);
-
-  const playerMove = getMoveName(playerInput);
-
-  /*printMessage("Your move is: " + playerMove);*/
-
-  const displayResult = function(argComputerMove, argPlayerMove) {
-    printMessage("I played " + argComputerMove + ", and you " + argPlayerMove);
-    if (
-      (computerMove == "rock" && playerMove == "paper") ||
-      (computerMove == "scissors" && playerMove == "rock") ||
-      (computerMove == "paper" && playerMove == "scissors")
-    ) {
-      printMessage("You Win");
-      updateScore.pScore++;
-    } else if (
-      (computerMove == "paper" && playerMove == "rock") ||
-      (computerMove == "rock" && playerMove == "scissors") ||
-      (computerMove == "scissors" && playerMove == "paper")
-    ) {
-      printMessage("I Win");
-      updateScore.cScore++;
-    } else if (computerMove === playerMove) {
-      printMessage("It's A Draw");
-    }
+    //Update Score
+    score.innerHTML = `<h2>Player: ${updateScore.pScore}</h2>
+    <h2>Computer: ${updateScore.cScore}</h2>`;
   };
-  displayResult(computerMove, playerMove);
-  //Update Score
 
-  score.innerHTML = `<h2>Player: ${updateScore.pScore}</h2>
-  <h2>Computer: ${updateScore.cScore}</h2>`;
+  // Restart game
+  const restartGame = function() {
+    const restart = document.getElementById("restart");
+    updateScore.pScore = 0;
+    updateScore.cScore = 0;
+    score.innerHTML = `<h2>Player: 0</h2><h2>Computer: 0</h2>`;
+    messages.innerHTML = `Choose an option`;
+  };
+
+  document.getElementById("play-rock").addEventListener("click", function() {
+    playGame("1");
+  });
+  document.getElementById("play-paper").addEventListener("click", function() {
+    playGame("2");
+  });
+  document
+    .getElementById("play-scissors")
+    .addEventListener("click", function() {
+      playGame("3");
+    });
+  document.getElementById("restart").addEventListener("click", function() {
+    restartGame();
+  });
 };
-
-// Restart game
-function restartGame() {
-  updateScore.pScore = 0;
-  updateScore.cScore = 0;
-  score.innerHTML = `
-  <h2>Player: 0</h2>
-  <h2>Computer: 0</h2>
-  `;
-  messages.innerHTML = `Choose an option`;
-}
-
-document.getElementById("play-rock").addEventListener("click", function() {
-  playGame("1");
-});
-document.getElementById("play-paper").addEventListener("click", function() {
-  playGame("2");
-});
-document.getElementById("play-scissors").addEventListener("click", function() {
-  playGame("3");
-});
-document.getElementById("restart").addEventListener("click", function() {
-  restartGame();
-});
+game();
