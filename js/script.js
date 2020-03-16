a470f93ed95f8f2369973bea3987b350715c72f2
Malgorzata Kolakowska <m.a.kolakowska@googlemail.com>
2020-03-16 14:57:59 +0000

    Change the function to the const.

--- js/script.js
+++ js/script.js
@@ -1,61 +1,43 @@
-// Get the modal
+/* References to HTML */
 
-let modal = document.getElementById("read-modal");
-
-// Get the button that opens the modal
-
-let btn = document.getElementById("read");
-
-// Get the <span> element that closes the modal
-
-let span = document.getElementsByClassName("close")[0];
+const gameInfoModal = document.querySelector("#game-info");
+const showGameInfoBtn = document.querySelector("#get-started");
+const closeGameInfoBtn = document.querySelector(".btn-close");
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-};
+showGameInfoBtn.addEventListener("click", () => {
+  gameInfoModal.style.display = "block";
+});
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-};
+closeGameInfoBtn.addEventListener("click", () => {
+  gameInfoModal.style.display = "none";
+});
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.addEventListener("click", event => {
+  if (event.target == gameInfoModal) {
+    gameInfoModal.style.display = "none";
   }
-};
+});
 
-//Game
+/* Game */
 const game = () => {
-  const score = document.getElementById("score");
-
-  const updateScore = {
-    pScore: 0,
-    cScore: 0
+  const scoreLabel = document.querySelector("#score");
+  const score = {
+    player: 0,
+    computer: 0
   };
 
   //Play Game
-  const playGame = function(playerInput) {
-    clearMessages();
-    const getMoveName = function(argMoveId) {
-      if (argMoveId == 1) {
-        return "rock";
-      } else if (argMoveId == 2) {
-        return "paper";
-      } else if (argMoveId == 3) {
-        return "scissors";
-      }
+  const playGame = playerInput => {
+    const getMoveName = argMoveId => {
+      if (argMoveId == 1) return "rock";
+      else if (argMoveId == 2) return "paper";
+      else if (argMoveId == 3) return "scissors";
     };
 
-    //Computer Options
-    const randomNumber = Math.floor(Math.random() * 3 + 1);
-    const computerMove = getMoveName(randomNumber);
-    const playerMove = getMoveName(playerInput);
-
-    //Compare Hands
-    const displayResult = function(argComputerMove, argPlayerMove) {
+    const displayResult = (argComputerMove, argPlayerMove) => {
       printMessage(
         "I played " + argComputerMove + ", and you " + argPlayerMove
       );
@@ -65,46 +47,45 @@ const game = () => {
         (computerMove == "paper" && playerMove == "scissors")
       ) {
         printMessage("You Win");
-        updateScore.pScore++;
-      } else if (
-        (computerMove == "paper" && playerMove == "rock") ||
-        (computerMove == "rock" && playerMove == "scissors") ||
-        (computerMove == "scissors" && playerMove == "paper")
-      ) {
-        printMessage("I Win");
-        updateScore.cScore++;
+        score.player++;
       } else if (computerMove === playerMove) {
         printMessage("It's A Draw");
+      } else {
+        printMessage("I Win");
+        score.computer++;
       }
     };
+
+    // Game round
+    clearMessages();
+
+    const computerMove = getMoveName(Math.floor(Math.random() * 3 + 1));
+    const playerMove = getMoveName(playerInput);
+
     displayResult(computerMove, playerMove);
 
-    //Update Score
-    score.innerHTML = `<h2>Player: ${updateScore.pScore}</h2>
-    <h2>Computer: ${updateScore.cScore}</h2>`;
+    scoreLabel.innerHTML = `<h2>Player: ${score.player}</h2>
+    <h2>Computer: ${score.computer}</h2>`;
   };
 
   // Restart game
-  const restartGame = function() {
-    const restart = document.getElementById("restart");
-    updateScore.pScore = 0;
-    updateScore.cScore = 0;
-    score.innerHTML = `<h2>Player: 0</h2><h2>Computer: 0</h2>`;
+  const restartGame = () => {
+    score.player = 0;
+    score.computer = 0;
+    scoreLabel.innerHTML = `<h2>Player: 0</h2><h2>Computer: 0</h2>`;
     messages.innerHTML = `Choose an option`;
   };
 
-  document.getElementById("play-rock").addEventListener("click", function() {
+  document.getElementById("play-rock").addEventListener("click", () => {
     playGame("1");
   });
-  document.getElementById("play-paper").addEventListener("click", function() {
+  document.getElementById("play-paper").addEventListener("click", () => {
     playGame("2");
   });
-  document
-    .getElementById("play-scissors")
-    .addEventListener("click", function() {
-      playGame("3");
-    });
-  document.getElementById("restart").addEventListener("click", function() {
+  document.getElementById("play-scissors").addEventListener("click", () => {
+    playGame("3");
+  });
+  document.getElementById("btn-restart").addEventListener("click", () => {
     restartGame();
   });
 };
